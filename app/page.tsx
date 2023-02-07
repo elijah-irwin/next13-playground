1854387928eb248ae9244964131d82c18ca79768
Mckenzie Irwin <irwinmck@gmail.com>
2023-02-06 22:17:47 -0500

    explored/added contentlayer as a mdx parser

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import { getPostMetaData } from '@/lib/posts';
 import PostPreview from '@/components/PostPreview';
 import PageTransition from '@/components/PageTransition';
 
+// Content.
+import { useMDXComponent } from 'next-contentlayer/hooks';
+import { allPosts } from 'contentlayer/generated';
+
 /****************************************
  * - Home Page -
  ***************************************/
@@ -13,10 +17,22 @@ export default function Home() {
     <PageTransition>
       <main>
         <h1 className='mb-5 text-2xl text-blue-400'>Home</h1>
-        <div className='flex flex-col gap-5'>
+        <div className='mb-5 flex flex-col gap-5'>
           {postMetaData.map(post => (
             <PostPreview key={post.slug} post={post} />
           ))}
+        </div>
+        <div className='mb-5 flex flex-col gap-5'>
+          {allPosts.map(post => {
+            const Component = useMDXComponent(post.body.code);
+            return (
+              <div key={post._id}>
+                <div>{post.title}</div>
+                <div>{post.date}</div>
+                <Component />
+              </div>
+            );
+          })}
         </div>
       </main>
     </PageTransition>
